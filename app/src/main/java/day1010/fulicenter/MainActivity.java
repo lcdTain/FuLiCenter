312bc37d1f471a77e2bf0d01b43813ca893502b3
lcdTain <lcd@18798828717@163.com>
2016-10-14 16:11:09 +0800

    add utils -- L and in the MainActivity test

--- app/src/main/java/day1010/fulicenter/MainActivity.java
+++ app/src/main/java/day1010/fulicenter/MainActivity.java
@@ -3,11 +3,14 @@ package day1010.fulicenter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import day1010.fulicenter.utils.L;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        L.i("main","onCreate");
     }
 }
