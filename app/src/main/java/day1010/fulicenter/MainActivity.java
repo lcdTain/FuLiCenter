b547953d443d873bd3e7ff3f0934b78347730cf2
lcdTain <lcd@18798828717@163.com>
2016-10-17 10:33:33 +0800

    add butterknife-plugin.jar and add dependence,realize bottom menu mutex

--- app/src/main/java/day1010/fulicenter/MainActivity.java
+++ app/src/main/java/day1010/fulicenter/MainActivity.java
@@ -1,20 +1,79 @@
 package day1010.fulicenter;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import day1010.fulicenter.utils.L;
 
 public class MainActivity extends AppCompatActivity {
 
+    @Bind(R.id.rb_new_good)
+    RadioButton rbNewGood;
+    @Bind(R.id.rb_boutique)
+    RadioButton rbBoutique;
+    @Bind(R.id.rb_category)
+    RadioButton rbCategory;
+    @Bind(R.id.tvCartCount)
+    TextView tvCartCount;
+    @Bind(R.id.rb_cart)
+    RadioButton rbCart;
+    @Bind(R.id.rb_personal)
+    RadioButton rbPersonal;
+
+    int index;
+    RadioButton[] rbs;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        L.i("main","onCreate");
+        ButterKnife.bind(this);
+        L.i("main", "onCreate");
+        initView();
     }
-    public void onCheckedChange(View v){
 
+    private void initView() {
+        rbs = new RadioButton[5];
+        rbs[0] = rbNewGood;
+        rbs[1] = rbBoutique;
+        rbs[2] = rbCategory;
+        rbs[3] = rbCart;
+        rbs[4] = rbPersonal;
+    }
+
+    public void onCheckedChange(View v) {
+        switch (v.getId()) {
+            case R.id.rb_new_good:
+                index = 0;
+                break;
+            case R.id.rb_boutique:
+                index = 1;
+                break;
+            case R.id.rb_category:
+                index = 2;
+                break;
+            case R.id.rb_cart:
+                index = 3;
+                break;
+            case R.id.rb_personal:
+                index = 4;
+                break;
+        }
+        setRadioButtonStatus();
+    }
+
+    private void setRadioButtonStatus() {
+        for (int i = 0; i < rbs.length; i++) {
+            if (i == index) {
+                rbs[i].setChecked(true);
+            } else {
+                rbs[i].setChecked(false);
+            }
+        }
     }
 }
