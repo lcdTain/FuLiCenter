8a37bb2dc8be47d220dd5c2f77fb9631625c6173
lcdTain <lcd@18798828717@163.com>
2016-10-19 13:12:53 +0800

    添加精品碎片

---
+++ app/src/main/java/day1010/fulicenter/fragment/BoutiqueFragment.java
@@ -0,0 +1,127 @@
+package day1010.fulicenter.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import day1010.fulicenter.Adapter.BoutiqueAdapter;
+import day1010.fulicenter.I;
+import day1010.fulicenter.R;
+import day1010.fulicenter.activity.MainActivity;
+import day1010.fulicenter.bean.BoutiqueBean;
+import day1010.fulicenter.bean.NewGoodsBean;
+import day1010.fulicenter.net.NetDao;
+import day1010.fulicenter.utils.CommonUtils;
+import day1010.fulicenter.utils.ConvertUtils;
+import day1010.fulicenter.utils.OkHttpUtils;
+import day1010.fulicenter.view.SpaceItemDecoration;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BoutiqueFragment extends Fragment {
+
+
+    @Bind(R.id.tvRefreshHint)
+    TextView tvRefreshHint;
+    @Bind(R.id.rv)
+    RecyclerView rv;
+    @Bind(R.id.sl)
+    SwipeRefreshLayout sl;
+    LinearLayoutManager llm;
+    MainActivity context;
+    BoutiqueAdapter mAdapter;
+    ArrayList<BoutiqueBean> mList;
+
+    public BoutiqueFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_new_goods, container, false);
+        ButterKnife.bind(this, view);
+        context = (MainActivity) getContext();
+        mList = new ArrayList<>();
+        mAdapter = new BoutiqueAdapter(context,mList);
+        initView();
+        initData();
+        setListener();
+        return view;
+    }
+
+    private void setListener() {
+
+    }
+
+
+    private void initData() {
+        downloadBoutique(I.ACTION_DOWNLOAD);
+
+    }
+    private void downloadBoutique(final int action) {
+        NetDao.downloadBoutique(context, new OkHttpUtils.OnCompleteListener<BoutiqueBean[]>() {
+            @Override
+            public void onSuccess(BoutiqueBean[] result) {
+                sl.setRefreshing(false);
+                mAdapter.setMore(true);
+                tvRefreshHint.setVisibility(View.GONE);
+                if (result != null && result.length > 0){
+                    ArrayList<BoutiqueBean> list = ConvertUtils.array2List(result);
+                    if (action == I.ACTION_PULL_DOWN || action == I.ACTION_DOWNLOAD){
+                        mAdapter.initData(list);
+                    }else{
+                        mAdapter.addData(list);
+                    }
+                    if (list.size() < I.PAGE_SIZE_DEFAULT){
+                        mAdapter.setMore(false);
+                    }
+                }else{
+                    mAdapter.setMore(false);
+                }
+            }
+
+            @Override
+            public void onError(String error) {
+                sl.setRefreshing(false);
+                tvRefreshHint.setVisibility(View.GONE);
+                CommonUtils.showShortToast(error);
+            }
+        });
+    }
+
+    private void initView() {
+        sl.setColorSchemeColors(
+                getResources().getColor(R.color.google_red),
+                getResources().getColor(R.color.google_yellow),
+                getResources().getColor(R.color.google_green),
+                getResources().getColor(R.color.google_blue)
+        );
+        llm = new LinearLayoutManager(context);
+        rv.setLayoutManager(llm);
+        rv.setHasFixedSize(true);
+        rv.setAdapter(mAdapter);
+        rv.addItemDecoration(new SpaceItemDecoration(12));
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
+    }
+}
