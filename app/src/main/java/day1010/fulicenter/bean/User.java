1b4e0c633fcc108c1d489ed8a89cf3cbb17fa019
lcdTain <lcd@18798828717@163.com>
2016-10-24 12:38:34 +0800

    添加数据库操作的三个工具类

---
+++ app/src/main/java/day1010/fulicenter/bean/User.java
@@ -0,0 +1,107 @@
+package day1010.fulicenter.bean;
+
+/**
+ * Created by Administrator on 2016/10/13.
+ */
+public class User {
+
+    /**
+     * muserName : a952702
+     * muserNick : 彭鹏
+     * mavatarId : 74
+     * mavatarPath : user_avatar
+     * mavatarSuffix : .jpg
+     * mavatarType : 0
+     * mavatarLastUpdateTime : 1476285149669
+     */
+
+    private String muserName;
+    private String muserNick;
+    private int mavatarId;
+    private String mavatarPath;
+    private String mavatarSuffix;
+    private int mavatarType;
+    private String mavatarLastUpdateTime;
+
+    public User() {
+    }
+
+    public User(String muserName, String muserNick, int mavatarId, String mavatarPath, String mavatarSuffix, int mavatarType, String mavatarLastUpdateTime) {
+        this.muserName = muserName;
+        this.muserNick = muserNick;
+        this.mavatarId = mavatarId;
+        this.mavatarPath = mavatarPath;
+        this.mavatarSuffix = mavatarSuffix;
+        this.mavatarType = mavatarType;
+        this.mavatarLastUpdateTime = mavatarLastUpdateTime;
+    }
+
+    public String getMuserName() {
+        return muserName;
+    }
+
+    public void setMuserName(String muserName) {
+        this.muserName = muserName;
+    }
+
+    public String getMuserNick() {
+        return muserNick;
+    }
+
+    public void setMuserNick(String muserNick) {
+        this.muserNick = muserNick;
+    }
+
+    public int getMavatarId() {
+        return mavatarId;
+    }
+
+    public void setMavatarId(int mavatarId) {
+        this.mavatarId = mavatarId;
+    }
+
+    public String getMavatarPath() {
+        return mavatarPath;
+    }
+
+    public void setMavatarPath(String mavatarPath) {
+        this.mavatarPath = mavatarPath;
+    }
+
+    public String getMavatarSuffix() {
+        return mavatarSuffix;
+    }
+
+    public void setMavatarSuffix(String mavatarSuffix) {
+        this.mavatarSuffix = mavatarSuffix;
+    }
+
+    public int getMavatarType() {
+        return mavatarType;
+    }
+
+    public void setMavatarType(int mavatarType) {
+        this.mavatarType = mavatarType;
+    }
+
+    public String getMavatarLastUpdateTime() {
+        return mavatarLastUpdateTime;
+    }
+
+    public void setMavatarLastUpdateTime(String mavatarLastUpdateTime) {
+        this.mavatarLastUpdateTime = mavatarLastUpdateTime;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "muserName='" + muserName + '\'' +
+                ", muserNick='" + muserNick + '\'' +
+                ", mavatarId=" + mavatarId +
+                ", mavatarPath='" + mavatarPath + '\'' +
+                ", mavatarSuffix='" + mavatarSuffix + '\'' +
+                ", mavatarType=" + mavatarType +
+                ", mavatarLastUpdateTime='" + mavatarLastUpdateTime + '\'' +
+                '}';
+    }
+}
---
+++ app/src/main/java/day1010/fulicenter/dao/DBManager.java
@@ -0,0 +1,75 @@
+package day1010.fulicenter.dao;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import day1010.fulicenter.I;
+import day1010.fulicenter.bean.User;
+
+/**
+ * Created by Administrator on 2016/10/24.
+ */
+public class DBManager {
+    private static DBManager dbManager = new DBManager();
+    private static DBOpenHelper mHelper;
+
+    void onInit(Context context){
+        mHelper = new DBOpenHelper(context);
+    }
+    public static synchronized  DBManager getInstance(){
+        return dbManager;
+    }
+    public synchronized  void closeDB(){
+        if (mHelper != null){
+            mHelper.closeDB();
+        }
+    }
+    public synchronized boolean saveUser(User user){
+        SQLiteDatabase db = mHelper.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(UserDao.TABLE_USER_NAME,user.getMuserName());
+        values.put(UserDao.TABLE_COLUMN_NICK,user.getMuserNick());
+        values.put(UserDao.TABLE_COLUMN_AVATAR_ID,user.getMavatarId());
+        values.put(UserDao.TABLE_COLUMN_AVATAR_TYPE,user.getMavatarType());
+        values.put(UserDao.TABLE_COLUMN_AVATAR_PATH,user.getMavatarPath());
+        values.put(UserDao.TABLE_COLUMN_AVATAR_SUFFIX,user.getMavatarSuffix());
+        values.put(UserDao.TABLE_COLUMN_AVATAR_LASTUPDATE_TIME,user.getMavatarLastUpdateTime());
+        if (db.isOpen()){
+            return db.replace(UserDao.TABLE_USER_NAME,null,values)!=-1;
+        }
+        return false;
+    }
+    public synchronized User getUser(String username){
+     SQLiteDatabase db = mHelper.getReadableDatabase();
+        String sql = "select * from " + UserDao.TABLE_USER_NAME + " where "
+                + UserDao.TABLE_COLUMN_NAME + " =?";
+        User user = null;
+        Cursor cursor = db.rawQuery(sql,new String[]{username});
+        if (cursor.moveToNext()){
+            user = new User();
+            user.setMuserName(username);
+            user.setMuserNick(cursor.getString(cursor.getColumnIndex(UserDao.TABLE_COLUMN_NICK)));
+            user.setMavatarId(cursor.getInt(cursor.getColumnIndex(UserDao.TABLE_COLUMN_AVATAR_ID)));
+            user.setMavatarType(cursor.getInt(cursor.getColumnIndex(UserDao.TABLE_COLUMN_AVATAR_TYPE)));
+            user.setMavatarPath(cursor.getString(cursor.getColumnIndex(UserDao.TABLE_COLUMN_AVATAR_PATH)));
+            user.setMavatarSuffix(cursor.getString(cursor.getColumnIndex(UserDao.TABLE_COLUMN_AVATAR_SUFFIX)));
+            user.setMavatarLastUpdateTime(cursor.getString(cursor.getColumnIndex(UserDao.TABLE_COLUMN_AVATAR_LASTUPDATE_TIME)));
+            return user;
+        }
+        return user;
+    }
+    public synchronized boolean updateUser(User user){
+        int result = -1;
+        SQLiteDatabase db = mHelper.getWritableDatabase();
+        String sql = UserDao.TABLE_COLUMN_NAME + "=?";
+        ContentValues values = new ContentValues();
+        values.put(UserDao.TABLE_COLUMN_NICK,user.getMuserNick());
+        if (db.isOpen()){
+            result = db.update(UserDao.TABLE_USER_NAME,values,sql,new String[]{user.getMuserName()});
+        }
+        return result > 0;
+
+    }
+}
