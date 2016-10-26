dac23e31aad4b7c658035d680107848ab52b1f39
lcdTain <lcd@18798828717@163.com>
2016-10-26 18:36:26 +0800

    添加分享的点击事件并实现分享功能

--- app/src/main/java/day1010/fulicenter/activity/GoodsDetailActivity.java
+++ app/src/main/java/day1010/fulicenter/activity/GoodsDetailActivity.java
@@ -8,6 +8,8 @@ import android.widget.TextView;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.onekeyshare.OnekeyShare;
 import day1010.fulicenter.FuLiCenterApplication;
 import day1010.fulicenter.I;
 import day1010.fulicenter.R;
@@ -121,15 +123,16 @@ public class GoodsDetailActivity extends BaseActivity {
     public void onBackClick() {
         MFGT.finish(context);
     }
-    private void isCollected(){
+
+    private void isCollected() {
         User user = FuLiCenterApplication.getUser();
-        if(user != null){
+        if (user != null) {
             NetDao.isCollect(context, user.getMuserName(), goodsID, new OkHttpUtils.OnCompleteListener<MessageBean>() {
                 @Override
                 public void onSuccess(MessageBean result) {
-                    if (result != null && result.isSuccess()){
+                    if (result != null && result.isSuccess()) {
                         isCollected = true;
-                    }else{
+                    } else {
                         isCollected = false;
                     }
                     updateGoodsCollectStatus();
@@ -146,9 +149,9 @@ public class GoodsDetailActivity extends BaseActivity {
     }
 
     private void updateGoodsCollectStatus() {
-        if (isCollected){
+        if (isCollected) {
             ivCollectIn.setImageResource(R.mipmap.bg_collect_out);
-        }else{
+        } else {
             ivCollectIn.setImageResource(R.mipmap.bg_collect_in);
         }
 
@@ -159,17 +162,18 @@ public class GoodsDetailActivity extends BaseActivity {
         super.onResume();
         isCollected();
     }
+
     @OnClick(R.id.ivCollectIn)
-    public void onCollectClick(){
+    public void onCollectClick() {
         User user = FuLiCenterApplication.getUser();
-        if (user == null){
+        if (user == null) {
             MFGT.gotoLoginActivity(context);
-        }else{
-            if (isCollected){
+        } else {
+            if (isCollected) {
                 NetDao.deleteCollect(context, user.getMuserName(), goodsID, new OkHttpUtils.OnCompleteListener<MessageBean>() {
                     @Override
                     public void onSuccess(MessageBean result) {
-                        if (result != null && result.isSuccess()){
+                        if (result != null && result.isSuccess()) {
                             isCollected = !isCollected;
                             updateGoodsCollectStatus();
                             CommonUtils.showLongToast(result.getMsg());
@@ -181,11 +185,11 @@ public class GoodsDetailActivity extends BaseActivity {
 
                     }
                 });
-            }else{
+            } else {
                 NetDao.addCollect(context, user.getMuserName(), goodsID, new OkHttpUtils.OnCompleteListener<MessageBean>() {
                     @Override
                     public void onSuccess(MessageBean result) {
-                        if (result != null && result.isSuccess()){
+                        if (result != null && result.isSuccess()) {
                             isCollected = !isCollected;
                             updateGoodsCollectStatus();
                             CommonUtils.showLongToast(result.getMsg());
@@ -201,4 +205,36 @@ public class GoodsDetailActivity extends BaseActivity {
 
         }
     }
+
+    @OnClick(R.id.ivShareNor)
+        public void showShare() {
+            ShareSDK.initSDK(this);
+            OnekeyShare oks = new OnekeyShare();
+            //关闭sso授权
+            oks.disableSSOWhenAuthorize();
+
+// 分享时Notification的图标和文字  2.5.9以后的版本不调用此方法
+            //oks.setNotification(R.drawable.ic_launcher, getString(R.string.app_name));
+            // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
+            oks.setTitle("标题");
+            // titleUrl是标题的网络链接，仅在人人网和QQ空间使用
+            oks.setTitleUrl("http://sharesdk.cn");
+            // text是分享文本，所有平台都需要这个字段
+            oks.setText("我是分享文本");
+            //分享网络图片，新浪微博分享网络图片需要通过审核后申请高级写入接口，否则请注释掉测试新浪微博
+            oks.setImageUrl("http://f1.sharesdk.cn/imgs/2014/02/26/owWpLZo_638x960.jpg");
+            // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+            //oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+            // url仅在微信（包括好友和朋友圈）中使用
+            oks.setUrl("http://sharesdk.cn");
+            // comment是我对这条分享的评论，仅在人人网和QQ空间使用
+            oks.setComment("我是测试评论文本");
+            // site是分享此内容的网站名称，仅在QQ空间使用
+            oks.setSite("ShareSDK");
+            // siteUrl是分享此内容的网站地址，仅在QQ空间使用
+            oks.setSiteUrl("http://sharesdk.cn");
+
+// 启动分享GUI
+            oks.show(this);
+        }
 }
